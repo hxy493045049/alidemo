b6a56febee48b4dfc16009e9fd4adb62be08e19e
shawn <shawn0729h@gmail.com>
2020-11-03 21:47:02 +0800

    none

---
+++ ApsaraVideoPlayer/Aliyunplayer/AlivcPlayerTools/src/main/java/com/aliyun/player/alivcplayerexpand/widget/TempUtils.kt
@@ -0,0 +1,17 @@
+package com.aliyun.player.alivcplayerexpand.widget
+
+import android.content.Context
+import java.io.File
+
+class TempUtils {
+//    fun getFileFromAssets(context: Context, fileName: String): File =
+//        File(context.cacheDir, fileName).also {
+//            if (!it.exists()) {
+//                it.outputStream().use { cache ->
+//                    context.assets.open(fileName).use { inputStream ->
+//                        inputStream.copyTo(cache)
+//                    }
+//                }
+//            }
+//        }
+}
